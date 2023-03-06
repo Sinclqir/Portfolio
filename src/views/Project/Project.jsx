e1024bbf2daf4f92eb5bc2ab747a58ee8399b542
Sinclair <95626827+Sinclqir@users.noreply.github.com>
2023-03-06 11:57:40 +0100

    Update Project.jsx

--- src/views/Project/Project.jsx
+++ src/views/Project/Project.jsx
@@ -10,7 +10,7 @@ import img1 from  '../../images/comparaisonTri.png'
 import img2 from  '../../images/Portfolio.png'
 import img3 from  '../../images/appolox.png'
 import img4 from  '../../images/other.png'
-
+import img5 from  '../../images/viapass.png'
 
 function Project() {
   useEffect(() =>{
@@ -27,6 +27,7 @@ function Project() {
             <ProjectBox title = "Sorting comparaison algorithm" description = "Technologies utilisées: JavaScript, HTML, CSS" author="Réalisé en groupe" time= "Temps passé: 4 mois"   img= {img1} link = "https://github.com/Sinclqir/Projet-Tutor-S4T"  />
             <ProjectBox title = "Portfolio Website" description = "Technologies utilisées: JavaScript, HTML, CSS, ReactJS" author="Réalisé en autonomie" time= "Temps passé: 1 semaine" img= {img2} link = "https://github.com/Sinclqir/Portfolio"  />
             <ProjectBox title = "Appolo X" description = "Technologies utilisées: JavaScript, HTML, CSS, ReactJS" author="Réalisé en groupe" time= "Temps passé: 4 mois" img= {img3} link = "https://github.com/Appolo-X/Site_web"  />
+            <ProjectBox title = "Stage chez viapass" description = "Technologies utilisées: JavaScript, PHP" author="Réalisé en autonomie" time= "Temps passé: 3 mois" img= {img5} link = "https://drive.google.com/drive/folders/1QCekGhNtKRUlItDrfIwag6B3U4chZC2C?usp=sharing"  />
             <ProjectBox title = "Other" img= {img4} link = "https://github.com/Sinclqir?tab=repositories"  />
           </div>
         </div>
