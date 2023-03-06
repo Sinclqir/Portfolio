20f2dd4a2e705a7d4d26001475bb20e00f184ed1
Sinclair <95626827+Sinclqir@users.noreply.github.com>
2023-03-06 10:13:29 +0100

    Update Project.jsx

--- src/views/Project/Project.jsx
+++ src/views/Project/Project.jsx
@@ -23,7 +23,7 @@ function Project() {
         <div className="boxes">
           <div className="carousel owl-carousel">
             <ProjectBox title = "Sorting comparaison algorithm" description = "Technologies utilisées: JavaScript, HTML, CSS" author="Réalisé en groupe" time= "Temps passé: 4 mois"   img= {img1} link = "https://github.com/Sinclqir/Projet-Tutor-S4T"  />
-            <ProjectBox title = "Portfolio Website" description = "Technologies utilisées: JavaScript, HTML, CSS, ReactJS" author="Réalisé en autonomie" time= "Temps passé: 1 semaine" img= {img2} link = "https://github.com/"  />
+            <ProjectBox title = "Portfolio Website" description = "Technologies utilisées: JavaScript, HTML, CSS, ReactJS" author="Réalisé en autonomie" time= "Temps passé: 1 semaine" img= {img2} link = "https://github.com/Sinclqir/Portfolio"  />
             <ProjectBox title = "Other" img= {img3} link = "https://github.com/Sinclqir?tab=repositories"  />
           </div>
         </div>
