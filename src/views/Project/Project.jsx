c9054220d3f9bc13688788f1fa7dfea4486e5129
Sinclair <95626827+Sinclqir@users.noreply.github.com>
2023-03-06 11:17:59 +0100

    Update Project.jsx

--- src/views/Project/Project.jsx
+++ src/views/Project/Project.jsx
@@ -8,7 +8,9 @@ import 'aos/dist/aos.css'
 import ProjectBox from '../../components/ProjectBox/ProjectBox'
 import img1 from  '../../images/comparaisonTri.png'
 import img2 from  '../../images/Portfolio.png'
-import img3 from  '../../images/other.png'
+import img3 from  '../../images/appolox.png'
+import img4 from  '../../images/other.png'
+
 
 function Project() {
   useEffect(() =>{
@@ -24,7 +26,8 @@ function Project() {
           <div className="carousel owl-carousel">
             <ProjectBox title = "Sorting comparaison algorithm" description = "Technologies utilisées: JavaScript, HTML, CSS" author="Réalisé en groupe" time= "Temps passé: 4 mois"   img= {img1} link = "https://github.com/Sinclqir/Projet-Tutor-S4T"  />
             <ProjectBox title = "Portfolio Website" description = "Technologies utilisées: JavaScript, HTML, CSS, ReactJS" author="Réalisé en autonomie" time= "Temps passé: 1 semaine" img= {img2} link = "https://github.com/Sinclqir/Portfolio"  />
-            <ProjectBox title = "Other" img= {img3} link = "https://github.com/Sinclqir?tab=repositories"  />
+            <ProjectBox title = "Appolo X" description = "Technologies utilisées: JavaScript, HTML, CSS, ReactJS" author="Réalisé en groupe" time= "Temps passé: 2 mois" img= {img3} link = "https://github.com/Appolo-X/Site_web"  />
+            <ProjectBox title = "Other" img= {img4} link = "https://github.com/Sinclqir?tab=repositories"  />
           </div>
         </div>
       </div>
